d9d3006909fe8fa47835c0ee9c6ba47a9032e5c6
scoomp <moshnyakov.alexei@yandex.ru>
2023-06-15 15:17:47 +0300

    6 commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import Header from "./Compnents/Header/Header";
-import Navbar from "./Compnents/Navbar/Navbar";
-import Profile from "./Compnents/Profile/Profile";
-import {BrowserRouter, Route} from "react-router-dom";
-import Dialogs from "./Compnents/Dialogs/Dialogs";
-import {message} from "antd";
+import {Header} from './components/Header/Header';
+import {Navbar} from './components/Navbar/Navbar';
+import {Profile} from './components/Profile/Profile';
+import {Dialogs} from './components/Dialogs/Dialogs';
+import {BrowserRouter, Route} from 'react-router-dom';
+import {News} from './components/News/News';
+import {Music} from './components/Music/Music';
+import {Settings} from './components/Settings/Settings';
+import {RootStateType} from './Redux/state';
 
-
-export function App() {
-
-    return (
-        <BrowserRouter>
-           <div className='app-wrapper'>
-               <Header/>
-               <Navbar/>
-
-            <div className='app-wrapper-content'>
-                <Route component={Dialogs}/>
-                <Route component={Profile}/>
-            </div>
-           </div>
-        </BrowserRouter>
-    );
+export type AppPropsType = {
+    state: RootStateType;
 }
 
 
+const App: React.FC<AppPropsType> = ({state}) => {
+
+
+    return (
+        <BrowserRouter>
+            <div className="app__wrapper">
+                <Header/>
+                <Navbar/>
+                <div className={'app__wrapper_content'}>
+                    <Route exact path={'/profile'} render={() => <Profile postsData={state.profilePage.postsData}/>}/>
+                    <Route exact path={'/dialogs'} render={() => <Dialogs dialogsData={state.dialogsPage.dialogsData} messagesData={state.dialogsPage.messagesData}/>}/>
+                    <Route exact path={'/news'} render={() => <News/>}/>
+                    <Route exact path={'/music'} render={() => <Music/>}/>
+                    <Route exact path={'/settings'} render={() => <Settings/>}/>
+                </div>
+            </div>
+        </BrowserRouter>
+    )
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {App} from './App';
+import App from './App';
+import "./reset.css";
+import state from './Redux/state';
+
+
 
 ReactDOM.render(
-    <App />,
+    <App state={state}/>,
   document.getElementById('root')
 );
